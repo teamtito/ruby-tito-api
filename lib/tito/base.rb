05eac52e1f1d65d9fe13f5752edbc6f9ce21c572
Paul Campbell <paul@rslw.com>
2015-12-09 17:52:45 +0000

    Allow setting the site via ENV var

--- lib/tito/base.rb
+++ lib/tito/base.rb
@@ -1,7 +1,5 @@
 module Tito
   class Base < JsonApiClient::Resource
-    # set the api base url in an abstract base class
-    # self.site = "https://api.tito.io/v2"
-    self.site = "http://api.tito.dev/v2"
+    self.site = ENV['TITO_SITE'] || "https://api.tito.io/v2"
   end
 end
