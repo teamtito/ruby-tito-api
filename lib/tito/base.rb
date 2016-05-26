851b9904cbd9de0504f14ffdfdb60811b2a890b4
Paul Campbell <paul@rslw.com>
2016-05-26 13:15:50 +0100

    Add  to be explicit about setting it

--- lib/tito/base.rb
+++ lib/tito/base.rb
@@ -1,5 +1,9 @@
 module Tito
   class Base < JsonApiClient::Resource
     self.site = ENV['TITO_SITE'] || "https://api.tito.io/v2"
+
+    def self.with_api_key(api_key)
+      with_params(api_key: api_key)
+    end
   end
 end
