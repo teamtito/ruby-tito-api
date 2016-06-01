1d964fd69d77a856244321b4d97a791d9f3488d7
Paul Campbell <paul@rslw.com>
2016-06-01 14:50:54 +0100

    define the site URL, don't set it

--- lib/tito/base.rb
+++ lib/tito/base.rb
@@ -1,6 +1,8 @@
 module Tito
   class Base < JsonApiClient::Resource
-    self.site = ENV['TITO_SITE'] || "https://api.tito.io/v2"
+    def self.site
+      ENV['TITO_SITE'] || "https://api.tito.io/v2"
+    end
 
     def self.with_api_key(api_key)
       with_params(api_key: api_key)
