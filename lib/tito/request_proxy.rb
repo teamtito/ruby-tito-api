be73f5fbae83bc0f5a504cab1f3f4b95c8a90186
Paul Campbell <paul@rslw.com>
2017-03-21 16:36:40 +0000

    allow api_key as a param for .all

--- lib/tito/request_proxy.rb
+++ lib/tito/request_proxy.rb
@@ -10,8 +10,9 @@ module Tito
       @api_key          = api_key
     end
 
-    def all
-      proxy_class.all(path_prefix: proxy_path, api_key: api_key)
+    def all(attrs = {})
+      _api_key = attrs.delete(:api_key) || api_key
+      proxy_class.all(path_prefix: proxy_path, api_key: _api_key)
     end
 
     def new(attrs = {})
